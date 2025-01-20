e1aef05eb0b3e538df70b082fd50b2cb266c8b57
Asbjoedt <asbjoern.skoedt@gmail.com>
2025-01-20 21:01:08 +0100

    commit

---
+++ app/board/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Board() {
+  return (
+  <main>
+    <div className="board">
+        <div className="board-section">
+            <div className="board-element">1</div>
+            <div className="board-element">2</div>
+            <div className="board-element">3</div>
+            <div className="board-element">4</div>
+            <div className="board-element">5</div>
+        </div>
+    </div>
+  </main>
+  );
+}
